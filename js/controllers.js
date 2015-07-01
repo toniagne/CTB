bcd2dba0223968f0e6f39da4787069d9682ad8a4
toniagne <toniagne@gmail.com>
2015-07-01 17:22:21 -0300

    CONTROLLER

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,8 @@
 angular.module('starter.controllers', ['ionic'])
 
-<<<<<<< HEAD
-.controller('ListaNoticias', function($scope, $http, $location, $ionicLoading, $ionicPopup) {
-=======
+
 .controller('DashCtrl', function($scope, $http,  $location, $ionicLoading, $ionicPopup) {
->>>>>>> origin/Inicial
+
 
 $ionicLoading.show({
     content: 'Loading',
@@ -68,7 +66,7 @@ $ionicLoading.show({
                 });  
           }
 
-	        $scope.converte = function(variavel) {
+          $scope.converte = function(variavel) {
             var str_1 = variavel.split("http://");
             if (!str_1[1]) { var str_2 = "";  } else { var str_2 = str_1[1].split('"');}
              
@@ -91,36 +89,8 @@ $ionicLoading.show({
     maxWidth: 200,
     showDelay: 0
   });
-<<<<<<< HEAD
-        $http.get('http://www.ctb.org.br/mobile/backend/ctb/noticias/135')
-=======
 
-  var headers = {
-        'Access-Control-Allow-Origin' : '*',
-        'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS, PUT',
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      };
-
-
-        $http.get('noticiasnacionais.json')
->>>>>>> origin/Inicial
-               .success(function(data) {
-                      $ionicLoading.hide();
-                })
-               .error(function(data) {
-                      $ionicLoading.hide();
-                  return $ionicPopup.alert({
-                       title: 'ATENÇÃO.',
-                       template: 'Seu dispositivo não esta conectado na internet.'
-                     });
-                })
-               .then(        
-                function(res){ 
-                  $scope.noticias  = res.data;              
-                });     
  
-        
          
         $scope.go = function(path){
          console.log(path);
@@ -141,14 +111,7 @@ $ionicLoading.show({
    
 })
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-<<<<<<< HEAD
-     
-   $scope.chat = Chats.get($stateParams.idNoticia, $stateParams.tiponoticia);
-=======
-  console.log(Chats.get($stateParams.idNoticia));
-
-   $scope.chat = Chats.get($stateParams.idNoticia);
->>>>>>> origin/Inicial
+ 
 })
 
 .controller('AccountCtrl', function($scope) {
